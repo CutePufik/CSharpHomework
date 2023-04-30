bd423253a49ec8c9d3a0caf4365f105a80839bab
Саня <ad.cherevko@gmail.com>
2023-04-30 22:00:28 +0300

    еще одно удаление моих локальных файлов- я удалю райдер

---
+++ Homework9/BinarySearchTree.cs
@@ -0,0 +1,83 @@
+﻿namespace BinaryTrees;
+
+public class BinarySearchTree
+{
+    private TreeNode<int> root;
+
+    public void Add(int x)
+    {
+        if (root == null)
+        {
+            root = new TreeNode<int>(x);
+            return;
+        }
+        add(root);
+
+        void add(TreeNode<int> node)
+        {
+            if (x < node.Data)
+            {
+                if (node.Left == null)
+                    node.Left = new TreeNode<int>(x);
+                else
+                    add(node.Left);
+            }
+            else
+            {
+                if (node.Right == null)
+                    node.Right = new TreeNode<int>(x);
+                else
+                    add(node.Right);
+            }
+        }
+    }
+
+
+
+    public BinarySearchTree(params int[] arr)
+    {
+        foreach (var number in arr)
+        {
+            Add(number);
+        }
+    }
+    
+  
+    /// log2 по сути
+    public bool Contains(int x)
+    {
+        var currentNode = root;
+        while (currentNode != null)
+        {
+            if (x == currentNode.Data)
+                return true;
+            currentNode = x < currentNode.Data ? currentNode.Left : currentNode.Right;
+        }
+        
+        return false;
+    }
+    
+
+    
+    public void PrintTreeInfix<T>()
+    {
+        TreeUtils.Infix(root);
+    }
+    
+    public void PrintTreePrefix<T>()
+    {
+        TreeUtils.Prefix(root);
+    }
+    
+    public void PrintTreePostfix<T>()
+    {
+        TreeUtils.Postfix(root);
+    }
+    
+    
+    
+    
+    
+    
+   
+}
---
+++ Homework9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BinaryTrees;
+
+
+public class myClass
+{
+
+    static void Main(string[] args)
+    {
+        //task 1
+        var bdp = new BinarySearchTree(1, 2, 3, 4, 5);
+        Console.WriteLine(bdp.Contains(5));
+        Console.WriteLine(bdp.Contains(-11));
+        bdp.Add(-11);
+        Console.WriteLine(bdp.Contains(-11));
+        
+        var tree = TreeUtils.GetSampleIntTree1();
+        TreeUtils.Prefix(tree);
+        Console.WriteLine();
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
